591b2283ddea9574ef4a09e1f09c43c3b774d708
andrewprog97 <andrewprog97@gmail.com>
2022-12-21 01:11:04 +0300

    add readDict function

--- project/Lab8.dpr
+++ project/Lab8.dpr
@@ -1,8 +1,32 @@
-program Lab8;
+﻿program Lab8;
 
 {$APPTYPE CONSOLE}
 
 {$R *.res}
+
+const MAX_LENGTH = 30;
+MAX_COUNT = 1000;
+
+type
+TDictArray = array[1..MAX_LENGTH, 1..MAX_COUNT] of string;
+TCountArray = array[1..MAX_LENGTH] of integer;
+
+function readDict(filePath: string): TDictArray;
+var dictArray: TDictArray;
+countArray: TCountArray;
+f: TextFile;
+buffer: string;
+begin
+    AssignFile(f, filePath);
+    reset(f);
+    while (not EOF(f)) do begin
+        Readln(f, buffer);
+        inc(countArray[length(buffer)]);
+        dictArray[length(buffer), countArray[length(buffer)]] := buffer;
+    end;
+    CloseFile(f);
+    readDict := dictArray;
+end;
 
 begin
 
