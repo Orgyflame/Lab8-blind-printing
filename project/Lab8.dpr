120c0ca5ca4ed9b5b536445e5422274f608b5ad4
andrewprog97 <andrewprog97@gmail.com>
2022-12-21 19:53:32 +0300

    add generateString function

--- project/Lab8.dpr
+++ project/Lab8.dpr
@@ -1,54 +1,104 @@
 ﻿program Lab8;
 
 {$APPTYPE CONSOLE}
-
 {$R *.res}
 
 uses Windows;
 
-const MAX_LENGTH = 30;
-MAX_COUNT = 1000;
+const
+  MAX_LENGTH = 15;
+  MAX_COUNT = 1000;
 
 type
-TDictArray = array[1..MAX_LENGTH, 1..MAX_COUNT] of string;
-TCountArray = array[1..MAX_LENGTH] of integer;
-TDict = record
+  TDictArray = array [1 .. MAX_LENGTH, 1 .. MAX_COUNT] of string;
+  TCountArray = array [1 .. MAX_LENGTH] of integer;
+
+  TDict = record
   var
-  words: TDictArray;
-  count: TCountArray;
+    words: TDictArray;
+    count: TCountArray;
+  end;
+
+function min(a, b: integer): integer;
+begin
+  if a < b then
+    result := a
+  else
+    result := b;
+end;
+
+function generateString(stringLength: integer; dict: TDict): string;
+var
+  wordLength: integer;
+  word: string;
+begin
+  result := '';
+
+  writeln(random(0));
+  repeat
+
+    repeat
+      wordLength := random(min(MAX_LENGTH, stringLength - length(result) - 1)) + 1;
+    until not(length(result) + 1 + wordLength + 1 = stringLength);
+
+
+    word := dict.words[wordLength][random(dict.count[wordLength]) + 1];
+
+    if length(result) = 0 then
+      result := word
+    else
+      result := result + ' ' + word;
+  until length(result) = stringLength;
 end;
 
 function readDict(filePath: string): TDict;
-var dictArray: TDictArray;
-countArray: TCountArray;
-f: TextFile;
-buffer: string;
+var
+  f: TextFile;
+  buffer: string;
+  i: integer;
 begin
-    AssignFile(f, filePath);
-    reset(f);
-    while (not EOF(f)) do begin
-        Readln(f, buffer);
-        inc(countArray[length(buffer)]);
-        dictArray[length(buffer), countArray[length(buffer)]] := UTF8Decode(buffer);
-    end;
-    CloseFile(f);
-    result.words := dictArray;
-    result.count := countArray;
+  AssignFile(f, filePath);
+  reset(f);
+
+  for i := 1 to MAX_LENGTH do
+     result.count[i] := 0;
+
+  while (not EOF(f)) do
+  begin
+    buffer := '';
+    Readln(f, buffer);
+    buffer := UTF8Decode(buffer);
+
+    inc(result.count[length(buffer)]);
+    result.words[length(buffer), result.count[length(buffer)]] := buffer;
+  end;
+
+  CloseFile(f);
 end;
 
-//var dict: TDict;
-//i, j: integer;
+var
+  dict: TDict;
+  i, j: integer;
 
 begin
-//    SetConsoleCP(1251);
-//    SetConsoleOutputCP(1251);
-//    dict := readDict('../../../dict-en.txt');  // 'C:\Универ\Lab8-blind-printing\dict-en.txt'
-//    for i := 1 to MAX_LENGTH do begin
-//        writeln(i);
-//        for j := 1 to dict.count[i] do begin
-//            write(dict.words[i, j], ' ');
-//        end;
-//        writeln;
+  SetConsoleCP(1251);
+  SetConsoleOutputCP(1251);
+  dict := readDict('../../../dict-en.txt');
+  // 'C:\Универ\Lab8-blind-printing\dict-en.txt'
+  for i := 1 to MAX_LENGTH do
+  begin
+//    if dict.count[i] = 0 then
+//      writeln(i);
+
+//    writeln(i);
+//    for j := 1 to dict.count[i] do
+//    begin
+//      write(dict.words[i, j], ' ');
 //    end;
-//    readln;
+//    writeln;
+
+  writeln(generateString(i, dict));
+  end;
+  Readln;
+
 end.
