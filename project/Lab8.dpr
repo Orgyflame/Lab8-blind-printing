ead2d6d704d3f65d741483bfca20c98b3470d288
andrewprog97 <andrewprog97@gmail.com>
2022-12-21 13:45:30 +0300

    refactor & fix bugs

--- project/Lab8.dpr
+++ project/Lab8.dpr
@@ -4,14 +4,21 @@
 
 {$R *.res}
 
+uses Windows;
+
 const MAX_LENGTH = 30;
 MAX_COUNT = 1000;
 
 type
 TDictArray = array[1..MAX_LENGTH, 1..MAX_COUNT] of string;
 TCountArray = array[1..MAX_LENGTH] of integer;
+TDict = record
+  var
+  words: TDictArray;
+  count: TCountArray;
+end;
 
-function readDict(filePath: string): TDictArray;
+function readDict(filePath: string): TDict;
 var dictArray: TDictArray;
 countArray: TCountArray;
 f: TextFile;
@@ -22,12 +29,26 @@ begin
     while (not EOF(f)) do begin
         Readln(f, buffer);
         inc(countArray[length(buffer)]);
-        dictArray[length(buffer), countArray[length(buffer)]] := buffer;
+        dictArray[length(buffer), countArray[length(buffer)]] := UTF8Decode(buffer);
     end;
     CloseFile(f);
-    readDict := dictArray;
+    result.words := dictArray;
+    result.count := countArray;
 end;
 
-begin
+//var dict: TDict;
+//i, j: integer;
 
+begin
+//    SetConsoleCP(1251);
+//    SetConsoleOutputCP(1251);
+//    dict := readDict('../../../dict-en.txt');  // 'C:\Универ\Lab8-blind-printing\dict-en.txt'
+//    for i := 1 to MAX_LENGTH do begin
+//        writeln(i);
+//        for j := 1 to dict.count[i] do begin
+//            write(dict.words[i, j], ' ');
+//        end;
+//        writeln;
+//    end;
+//    readln;
 end.
